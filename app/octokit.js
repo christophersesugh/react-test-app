e5a794d8fea68b3e3b7c65ec12e5eae1e49e3ce1
Christopher Sesugh <christophersesugh@Christophers-MacBook-Air.local>
2023-09-17 11:37:07 +0100

    add form

---
+++ app/octokit.js
@@ -0,0 +1,7 @@
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+  auth: "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",
+});
+
+export { octokit };
--- app/routes/_index.js
+++ app/routes/_index.js
@@ -1,5 +1,61 @@
 import React from "react";
+import { useFetcher } from "react-router-dom";
+import { octokit } from "../octokit";
+
+export async function action({ request }) {
+  const form = await request.formData();
+  const input = form.get("input");
+
+  try {
+    const response = await octokit.request(
+      "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+      {
+        owner: "christophersesugh",
+        repo: "react-test-app",
+        workflow_id: "main.yml",
+        ref: "main",
+        inputs: {
+          framework: input,
+        },
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+}
 
 export default function Index() {
-  return <div className="text-red-500 text-4xl">index</div>;
+  const fetcher = useFetcher();
+  return (
+    <main className="flex flex-col mx-auto max-w-2xl h-screen place-content-center bg-slate-100">
+      <h1 className="text-2xl text-blue-500 text-center">GitHub Actions</h1>
+      <div className="max-w-xl w-full mx-auto">
+        <fetcher.Form
+          action="/?index"
+          method="POST"
+          className="w-full mt-12 flex flex-col gap-4"
+        >
+          <h2 className="text-lg">FORM</h2>
+          <input
+            type="text"
+            name="input"
+            id="input"
+            className="p-2 rounded-md bg-white w-full"
+            placeholder="param"
+          />
+          <button
+            type="submit"
+            className="bg-purple-500 rounded-md p-2 text-white w-full"
+          >
+            submit
+          </button>
+        </fetcher.Form>
+      </div>
+    </main>
+  );
 }
